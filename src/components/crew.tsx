5d4b408c7722b3ea7dba882d78be4092a86303a8
MUZAAQI <muzaaqi@yahoo.com>
2025-11-27 22:56:50 +0700

    chore(ui): Adjust crew section grid width for responsiveness
    Updated the grid container's width to use w-10/12 on smaller screens and w-2/3 on medium screens, improving layout responsiveness and appearance.

--- src/components/crew.tsx
+++ src/components/crew.tsx
@@ -22,7 +22,7 @@ const CrewSection = () => {
   return (
     <div className="container flex min-h-svh flex-col items-center justify-center py-10">
       <h1 className="font-sans text-5xl font-bold">CREW</h1>
-      <div className="grid grid-cols-1 gap-10 sm:grid-cols-2 md:grid-cols-3 mt-10 w-2/3">
+      <div className="grid grid-cols-1 gap-10 sm:grid-cols-2 md:grid-cols-3 mt-10 w-10/12 md:w-2/3">
         {crewItems.map(({ name, role, img }) => (
           <Card key={name} className="">
             <CardContent className="relative overflow-hidden group aspect-3/4">
