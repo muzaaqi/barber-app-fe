e6f8652ea42a11f44fd3de404ec560e18bc1aefb
MUZAAQI <muzaaqi@yahoo.com>
2025-11-27 23:01:13 +0700

    chore(ui): Update card width and remove iframe height in location
    Adjusted the Card component width to be more responsive by using w-10/12 for small screens and md:w-2/3 for medium screens. Removed the fixed height attribute from the embedded Google Maps iframe for better adaptability.

--- src/components/location.tsx
+++ src/components/location.tsx
@@ -4,11 +4,10 @@ const LocationSection = () => {
   return (
     <div className="container flex min-h-svh flex-col items-center justify-center py-10">
       <h1 className="font-sans text-5xl font-bold">LOCATION</h1>
-      <Card className="mt-10 w-2/3">
+      <Card className="mt-10 w-10/12 md:w-2/3">
         <CardContent className="flex flex-col items-center">
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d440.1599142500016!2d109.92239018303175!3d-7.842449133285149!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7add001e0d0725%3A0xe3733fcaec2dd351!2sBERGAS%20BARBERSHOP!5e0!3m2!1sen!2sid!4v1764232590663!5m2!1sen!2sid"
-            height={450}
             style={{ border: 0 }}
             allowFullScreen={true}
             loading="lazy"
