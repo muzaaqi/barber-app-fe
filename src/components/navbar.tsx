a7375f2e15c271a21d1cda8716fd3485781b5471
MUZAAQI <muzaaqi@yahoo.com>
2025-11-27 22:43:22 +0700

    chore(nav): Update navbar padding and menu visibility
    Added horizontal padding to the navbar container and hid the navigation menu on small screens by applying responsive classes.

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,12 +4,12 @@ import ThemeSwitch from "./theme-switch";
 const Navbar = () => {
   return (
     <nav className="bg-background border-border fixed z-50 flex w-full justify-center border-b py-4">
-      <div className="container flex justify-between items-center font-mono">
+      <div className="container flex justify-between items-center font-mono px-5 md:px-0">
         <div className="text-primary text-2xl font-extrabold">
           <h1>BERGAS</h1>
         </div>
         <div className="text-lg font-medium flex items-center gap-6">
-          <ul className="flex gap-5">
+          <ul className="hidden md:flex gap-5 ">
             <li>
               <Link href="/">Home</Link>
             </li>
