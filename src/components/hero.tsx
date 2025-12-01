6d43a321d55bb3f414c005277b156aef0b659b48
MUZAAQI <muzaaqi@yahoo.com>
2025-12-01 10:06:35 +0700

    chore(ui): Refactor hero section layout and text styles
    Reordered Tailwind classes for consistency, adjusted heading punctuation and color, and improved button formatting for better readability and style alignment.

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -5,24 +5,26 @@ const HeroSection = () => {
   return (
     <div className="min-h-svh w-full">
       <Image
-        className="absolute z-0 min-h-svh max-h-svh min-w-full object-cover"
+        className="absolute z-0 max-h-svh min-h-svh min-w-full object-cover"
         src={"/hero-bg.jpg"}
         alt="Hero Background"
         width={1920}
         height={1080}
       />
-      <div className="relative z-10 flex min-h-svh flex-col items-center justify-center text-center bg-background/50">
-        <h1 className="font-sans text-4xl md:text-8xl font-bold">
-          HASIL RAPI.
+      <div className="bg-background/50 relative z-10 flex min-h-svh flex-col items-center justify-center text-center">
+        <h1 className="font-sans text-4xl font-bold md:text-8xl">
+          HASIL RAPI<span className="text-primary">.</span>
         </h1>
-        <h2 className="font-sans text-4xl md:text-8xl font-bold">
-          POTONGAN PRESISI.
+        <h2 className="font-sans text-4xl font-bold md:text-8xl">
+          POTONGAN PRESISI
         </h2>
         <div className="flex flex-col items-center gap-4">
           <span className="text-muted-foreground/80 md:text-lg">
             BARBER PROFESIONAL | TEMPAT NYAMAN
           </span>
-          <Button className="px-5 py-6 md:px-7 md:py-6 text-lg md:text-2xl">POTONG SEKARANG</Button>
+          <Button className="px-5 py-6 text-lg md:px-7 md:py-6 md:text-2xl">
+            POTONG SEKARANG
+          </Button>
         </div>
       </div>
     </div>
