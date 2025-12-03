ed96167dcddcd8713bfdbbf2ced037ce902b4135
MUZAAQI <muzaaqi@yahoo.com>
2025-12-03 23:03:39 +0700

    chore(api): Update Axios instance to use env API URL
    Renamed the Axios instance export and changed the baseURL to use the NEXT_PUBLIC_API_URL environment variable for improved configurability.

--- src/lib/axios-instance.ts
+++ src/lib/axios-instance.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const instance = axios.create({
-  baseURL: 'https://bergas-api.muzaaqi.my.id/api',
+export const BergasInstance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
 
