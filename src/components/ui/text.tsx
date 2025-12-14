f3b17a004036d9719fdb7db440ad33be9df81270
MUZAAQI <muzaaqi@yahoo.com>
2025-12-14 22:06:09 +0700

    refactor: Refactor SectionTitle and remove unused components
    Updated SectionTitle to center content and adjust heading sizes. Removed unused Section and SectionContent components for cleaner code.

--- src/components/ui/text.tsx
+++ src/components/ui/text.tsx
@@ -2,31 +2,9 @@ import React from "react";
 
 const SectionTitle = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="mb-5 flex items-center gap-4">
-      <div className="bg-primary size-6 lg:size-8 xl:size-10"></div>
-      <h1 className="text-xl font-bold lg:text-2xl xl:text-4xl">{children}</h1>
+    <div className="mb-5 flex items-center justify-center gap-4">
+      <h1 className="text-xl font-bold lg:text-3xl xl:text-5xl">{children}</h1>
     </div>
   );
 };
-
-const Section = ({
-  children,
-  className,
-}: {
-  children: React.ReactNode;
-  className?: string;
-}) => {
-  return <div className={`justify-center container ${className}`}>{children}</div>;
-};
-
-const SectionContent = ({
-  children,
-  className,
-}: {
-  children: React.ReactNode;
-  className?: string;
-}) => {
-  return <div className={`mt-5 ${className}`}>{children}</div>;
-};
-
 export default SectionTitle;
