c53167f2735bbfc8e77eebe7c91b55b61f5877c0
MUZAAQI <muzaaqi@yahoo.com>
2025-12-14 22:17:47 +0700

    refactor: Refactor Navbar layout for improved responsiveness
    Updated the Navbar component to use a grid layout on large screens and adjusted visibility of navigation and action items for better responsiveness. Moved AuthButton and ThemeSwitch to a dedicated section visible only on large screens, and updated breakpoints for menu display.

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -10,8 +10,7 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { Home, Info, Scissors, Menu, MapPin, Package } from "lucide-react";
 import { AuthButton, MobileAuthButton } from "./auth-button";
-
-const Navbar = () => {
+const Navbar = async () => {
   const liClass = "hover:text-primary transition-color duration-300";
   const navItems = [
     {
@@ -42,27 +41,21 @@ const Navbar = () => {
   ];
   return (
     <nav className="bg-background/50 border-border fixed z-50 flex w-full justify-center border-b px-5 py-4 backdrop-blur-sm xl:px-0">
-      <div className="container flex items-center justify-between font-mono">
+      <div className="container flex justify-between font-mono lg:grid lg:grid-cols-3">
         <div className="text-primary text-2xl font-extrabold">
           <Link href="/">
             <h1>BERGAS</h1>
           </Link>
         </div>
-        <div className="flex items-center gap-6 text-lg font-medium">
-          <ul className="hidden gap-5 md:flex">
+        <div className="flex items-center justify-center gap-6 text-lg font-medium">
+          <ul className="hidden gap-5 lg:flex">
             {navItems.map(({ name, url }) => (
               <li key={name} className={liClass}>
                 <Link href={url}>{name}</Link>
               </li>
             ))}
           </ul>
-          <div>
-          </div>
-          <div className="hidden gap-3 md:flex">
-            <AuthButton />
-            <ThemeSwitch />
-          </div>
-          <div className="md:hidden">
+          <div className="lg:hidden">
             <DropdownMenu>
               <DropdownMenuTrigger asChild>
                 <Menu />
@@ -85,12 +78,16 @@ const Navbar = () => {
                   <MobileThemeSwitch />
                 </DropdownMenuItem>
                 <DropdownMenuSeparator />
-                <DropdownMenuItem asChild>
+                <DropdownMenuItem>
                   <MobileAuthButton />
                 </DropdownMenuItem>
               </DropdownMenuContent>
             </DropdownMenu>
           </div>
+        </div>
+        <div className="hidden items-center justify-end gap-3 lg:flex">
+          <AuthButton />
+          <ThemeSwitch />
         </div>
       </div>
     </nav>
