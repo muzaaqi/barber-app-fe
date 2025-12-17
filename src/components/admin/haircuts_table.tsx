3385a921a2a4bf14779f17e73582efe5b7103f8c
MUZAAQI <muzaaqi@yahoo.com>
2025-12-17 18:26:29 +0700

    feat: Add GlobalPagination component
    Introduces a reusable GlobalPagination component for navigating paginated content. It generates page links based on current and total pages, and disables navigation when on the first or last page.

--- src/components/admin/haircuts_table.tsx
+++ src/components/admin/haircuts_table.tsx
@@ -16,6 +16,7 @@ import {
 import { Ellipsis, Trash } from "lucide-react";
 import { api } from "@/lib/axios-instance";
 import EditHaircut from "./edit-haircut";
+import GlobalPagination from "../global-pagination";
 
 type Haircut = {
   id: number;
@@ -27,50 +28,71 @@ type Haircut = {
 const HaircutsTable = async () => {
   const res = (await api.get("/haircuts")) || [];
   const haircuts: Haircut[] = res.data.data.data;
+  const pagination = res.data.data.pagination;
   return (
-    <Table>
-      <TableHeader>
-        <TableRow>
-          <TableCell>NO</TableCell>
-          <TableCell>Nama Model</TableCell>
-          <TableCell>Deskripsi</TableCell>
-          <TableCell>Gambar</TableCell>
-          <TableCell>Action</TableCell>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {haircuts.map(({ id, name, description, image_url }, index) => (
-          <TableRow key={id}>
-            <TableCell>{index + 1}</TableCell>
-            <TableCell>{name}</TableCell>
-            <TableCell>{description.length > 50 ? description.substring(0, 50) + "..." : description}</TableCell>
-            <TableCell>
-              <Popover>
-                <PopoverTrigger asChild>
-                  <Button variant="secondary">View Image</Button>
-                </PopoverTrigger>
-                <PopoverContent className="w-fit">
-                  <Image src={image_url} alt={name} width={200} height={200} />
-                </PopoverContent>
-              </Popover>
-            </TableCell>
-            <TableCell>
-              <DropdownMenu>
-                <DropdownMenuTrigger>
-                  <Ellipsis />
-                </DropdownMenuTrigger>
-                <DropdownMenuContent className="w-fit grid">
-                  <EditHaircut id={id} haircut_name={name} haircut_description={description} image_url={image_url} />
-                  <Button variant="ghost" className="justify-start gap-3">
-                    <Trash /> Delete
-                  </Button>
-                </DropdownMenuContent>
-              </DropdownMenu>
-            </TableCell>
+    <>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableCell>NO</TableCell>
+            <TableCell>Nama Model</TableCell>
+            <TableCell>Deskripsi</TableCell>
+            <TableCell>Gambar</TableCell>
+            <TableCell>Action</TableCell>
           </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+        </TableHeader>
+        <TableBody>
+          {haircuts.map(({ id, name, description, image_url }, index) => (
+            <TableRow key={id}>
+              <TableCell>{index + 1}</TableCell>
+              <TableCell>{name}</TableCell>
+              <TableCell>
+                {description.length > 50
+                  ? description.substring(0, 50) + "..."
+                  : description}
+              </TableCell>
+              <TableCell>
+                <Popover>
+                  <PopoverTrigger asChild>
+                    <Button variant="secondary">View Image</Button>
+                  </PopoverTrigger>
+                  <PopoverContent className="w-fit">
+                    <Image
+                      src={image_url}
+                      alt={name}
+                      width={200}
+                      height={200}
+                    />
+                  </PopoverContent>
+                </Popover>
+              </TableCell>
+              <TableCell>
+                <DropdownMenu>
+                  <DropdownMenuTrigger>
+                    <Ellipsis />
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent className="grid w-fit">
+                    <EditHaircut
+                      id={id}
+                      haircut_name={name}
+                      haircut_description={description}
+                      image_url={image_url}
+                    />
+                    <Button variant="ghost" className="justify-start gap-3">
+                      <Trash /> Delete
+                    </Button>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      <GlobalPagination
+        currentPage={pagination.page}
+        totalPages={pagination.total}
+      />
+    </>
   );
 };
 
