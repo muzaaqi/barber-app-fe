1fa43dcad3f84bcd7297bdaaa49f5a82fe30edad
MUZAAQI <muzaaqi@yahoo.com>
2025-12-18 19:37:23 +0700

    feat: Add getAuthHeader function to retrieve JWT token
    Introduces a utility function that fetches the JWT token from cookies using Next.js headers. This will help standardize token retrieval for authentication purposes.

---
+++ src/features/get-jwt-token.ts
@@ -0,0 +1,7 @@
+import { cookies } from "next/headers";
+
+export default async function getAuthHeader() {
+  const cookiesStore = await cookies();
+  const token = cookiesStore.get("token")?.value;
+  return token;
+}
