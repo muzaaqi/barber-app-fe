e57c38458a5c06caefcdfb8c4297a5f1e0dab97b
MUZAAQI <muzaaqi@yahoo.com>
2025-12-19 21:10:46 +0700

    feat: Enhance proxy with user role and auth checks
    The proxy function now fetches the user profile and checks both token and user presence for protected routes. It also restricts access to /dashboard to admin users only, redirecting unauthorized users to the home page.

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,29 +1,31 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import { getProfile } from "./actions/auth/get-profile";
 
-export function proxy(req: NextRequest) {
+export async function proxy(req: NextRequest) {
   const { nextUrl } = req;
   const token = req.cookies.get("token")?.value;
+  const user = await getProfile();
 
   const pathname = nextUrl.pathname;
 
-  const protectedRoutes = [
-    "/my-todos",
-  ];
+  const protectedRoutes = ["/my-todos"];
 
-  const authPath = [
-    "/login", "/signup",
-  ]
+  const authPath = ["/login", "/signup"];
 
   const isProtected = protectedRoutes.some((route) =>
-    pathname.startsWith(route)
+    pathname.startsWith(route),
   );
 
-  if (isProtected && !token) {
+  if (isProtected && (!token || !user)) {
     return NextResponse.redirect(new URL("/login", req.url));
   }
 
   if (authPath.some((route) => pathname.startsWith(route)) && token) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+
+  if (pathname.startsWith("/dashboard") && user?.role !== "admin") {
     return NextResponse.redirect(new URL("/", req.url));
   }
 
