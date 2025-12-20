48ef9033c62a3cd81a55098ea1d120ba4ccffd84
MUZAAQI <muzaaqi@yahoo.com>
2025-12-20 19:24:48 +0700

    feat: Add product detail page for user services
    Created ProductDetailPage component under the user services route to display product details using the ProductDetail component.

---
+++ src/app/(user)/services/product/[productId]/page.tsx
@@ -0,0 +1,11 @@
+import ProductDetail from "@/components/product-detail"
+
+const ProductDetailPage = () => {
+  return (
+    <div className="container items-center justify-center mx-auto px-5">
+      <ProductDetail />
+    </div>
+  )
+}
+
+export default ProductDetailPage
