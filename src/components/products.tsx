3f3baa9b133f64c7a0101592db48e66e7ef54f08
MUZAAQI <muzaaqi@yahoo.com>
2025-12-20 19:24:34 +0700

    refactor: Enhance product card with details and dialog
    Added product description, stock, and price to the Products component. Wrapped each product card in a Link to its detail page and replaced the 'Beli' button with a ProductDialog component for more detailed interaction. Improved UI by formatting price and adjusting button group behavior.

--- src/components/products.tsx
+++ src/components/products.tsx
@@ -7,15 +7,21 @@ import { Skeleton } from "./ui/skeleton";
 import { useEffect, useState } from "react";
 import { api } from "@/lib/axios-instance";
 import { toast } from "sonner";
+import ProductDialog from "./user/product-dialog";
+import { formatIDR } from "@/features/formatter";
+import Link from "next/link";
 
 type Products = {
   id: string;
   name: string;
+  description: string;
+  stock: number;
+  price: number;
   image_url: string;
 };
 
 const Products = () => {
-  const [products, setProducts ] = useState<Products[]>([]);
+  const [products, setProducts] = useState<Products[]>([]);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
@@ -32,41 +38,71 @@ const Products = () => {
       }
     };
     fetchProducts();
-  }, [])
+  }, []);
   return (
-    <div className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4">
-      {isLoading || !products ? (
-        Array.from({ length: 10 }).map((_, index) => (
-          <Card key={index}>
-            <CardContent className="text-center">
-              <Skeleton className="aspect-square mb-3"/>
-              <Skeleton className="h-10 w-full"/>
-            </CardContent>
-            <CardFooter>
-              <Skeleton className="w-full h-10"/>
-            </CardFooter>
-          </Card>
-        ))
-      ) : products.map(({ id, name, image_url }) => (
-        <Card key={id} className="hover:border-primary transition-colors duration-300">
-          <CardContent className="text-center">
-            <Image
-              src={image_url}
-              alt={name}
-              width={1000}
-              height={1000}
-              className=""
-            />
-            <h2 className="text-xl md:text-2xl font-semibold">{name}</h2>
-          </CardContent>
-          <CardFooter>
-          <ButtonGroup className="grid grid-cols-2 gap-2 w-full">
-              <Button variant="secondary"><ShoppingCart />Troli</Button>
-              <Button><ShoppingBag />Beli</Button>
-            </ButtonGroup>
-          </CardFooter>
-        </Card>
-      ))}
+    <div className="grid grid-cols-2 gap-4 md:grid-cols-3 xl:grid-cols-4">
+      {isLoading || !products
+        ? Array.from({ length: 10 }).map((_, index) => (
+            <Card key={index}>
+              <CardContent className="text-center">
+                <Skeleton className="mb-3 aspect-square" />
+                <Skeleton className="h-10 w-full" />
+              </CardContent>
+              <CardFooter>
+                <Skeleton className="h-10 w-full" />
+              </CardFooter>
+            </Card>
+          ))
+        : products.map(({ id, name, description, stock, price, image_url }) => (
+            <Link
+              href={`/services/product/${id}`}
+              key={id}
+              className="no-underline"
+            >
+              <Card
+                key={id}
+                className="hover:border-primary transition-colors duration-300"
+              >
+                <CardContent className="text-center">
+                  <Image
+                    src={image_url}
+                    alt={name}
+                    width={1000}
+                    height={1000}
+                    className=""
+                  />
+                  <div>
+                    <h2 className="text-xl font-semibold md:text-2xl">
+                      {name}
+                    </h2>
+                    <h2>{formatIDR(price)}</h2>
+                  </div>
+                </CardContent>
+                <CardFooter>
+                  <ButtonGroup
+                    className="grid w-full grid-cols-2 gap-2 pointer-events-none md:pointer-events-auto"
+                    onClick={(e) => {
+                      e.preventDefault();
+                      e.stopPropagation();
+                    }}
+                  >
+                    <Button variant="secondary">
+                      <ShoppingCart />
+                      Troli
+                    </Button>
+                    <ProductDialog
+                      id={id}
+                      name={name}
+                      description={description}
+                      stock={stock}
+                      price={price}
+                      image_url={image_url}
+                    />
+                  </ButtonGroup>
+                </CardFooter>
+              </Card>
+            </Link>
+          ))}
     </div>
   );
 };
