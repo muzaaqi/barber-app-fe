353b6e4b4302681d74c61615f085368c480768bd
MUZAAQI <muzaaqi@yahoo.com>
2025-12-20 20:06:02 +0700

    feat: Add getInitials utility and update exports in formatter
    Introduces a getInitials function to extract and format initials from a name string. Also updates the export statement to include both formatIDR and getInitials.

--- src/features/formatter.ts
+++ src/features/formatter.ts
@@ -1,7 +1,18 @@
-export const formatIDR = (amount: number): string => {
+const formatIDR = (amount: number): string => {
   return new Intl.NumberFormat("id-ID", {
     style: "currency",
     currency: "IDR",
     minimumFractionDigits: 0,
   }).format(amount);
 }
+
+const getInitials = (name: string) => {
+  return name
+    .split(' ')
+    .map((n) => n[0])
+    .join('')
+    .toUpperCase()
+    .substring(0, 2);
+};
+
+export { formatIDR, getInitials };
